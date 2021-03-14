0faac7144f0e5bfb9bec70d770e13e9b4101dc85
nesterov26 <53916689+nesterov26@users.noreply.github.com>
2021-03-14 16:45:51 +0200

    Create python.py

---
+++ python.py
@@ -0,0 +1,6 @@
+s = "I'm a candidate for devops position in Quantum Projects"
+
+parts = s.split(' ')
+
+
+print (parts[0] + "_" + parts[1] + "-" + parts[2] + "/" + parts[3] + "_" + parts[4] + "-" + parts[5] + "/" + parts[6] + "_" + parts[7] + "-" + parts[8])
